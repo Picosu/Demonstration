99dc04d8ce1f9dcfd3e4c976b5b05479bd1366a9
Maxence <maxence.decussac.ext@francetv.fr>
2015-10-14 11:56:32 +0200

    création de la vue d'ajout de cocktail

---
+++ Demonstration/CocktailSelectionVC.swift
@@ -0,0 +1,27 @@
+//
+//  CocktailSelectionVC.swift
+//  Demonstration
+//
+//  Created by Maxence DE CUSSAC on 14/10/2015.
+//  Copyright © 2015 Maxence de Cussac. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CocktailSelectionVC : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        for var view in self.view.subviews {
+//            if view.isKindOfClass(UIButton)
+//            {
+//                view.layer.cornerRadius = view.frame.size.width/2
+//                view.layer.borderWidth = 1
+//            }
+//        }
+    }
+    @IBAction func addDrink(sender: UIButton) {
+    }
+}
